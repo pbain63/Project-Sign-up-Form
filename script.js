7582a22017d6681aced95a2673dd1bd25f5d3200
pbain63 <prodipbain63@gmail.com>
2024-05-27 23:35:53 +0600

    Change the fontsize for better UI at validation textContent in script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+function validate_password() {
+  let password = document.getElementById("password").value;
+  let confirmPassword = document.getElementById("confirm-password").value;
+
+  if (password !== confirmPassword) {
+    document.getElementById("wrong_pass_alert").style.color = "red";
+    document.getElementById("wrong_pass_alert").style.fontSize = "10px";
+    document.getElementById("wrong_pass_alert").textContent =
+      "☒ Passwords are incorrect";
+
+    document.getElementById("create").disabled = true;
+    document.getElementById("create").style.opacity = 0.3;
+  } else {
+    document.getElementById("wrong_pass_alert").style.color = "green";
+    document.getElementById("wrong_pass_alert").style.fontSize = "10px";
+    document.getElementById("wrong_pass_alert").textContent =
+      "🗹 Password matched";
+
+    document.getElementById("create").disabled = false;
+    document.getElementById("create").style.opacity = 1;
+  }
+}
